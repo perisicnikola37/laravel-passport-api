f67e0d7c0d95a13a926bd17311f3775fb24eef14
Nikola Perisic <5dzoni5@gmail.com>
2022-06-25 00:24:39 +0200

    Passport Installed

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ use App\Http\Resources\ProductCollection;
 use App\Http\Resources\ProductResource;
 use App\Models\Product;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
