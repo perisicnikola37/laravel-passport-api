b95fdaa456cc000fedb53ef2fb43165a3992de64
Nikola Perisic <5dzoni5@gmail.com>
2022-06-30 01:33:22 +0200

    Review Update finished

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -81,9 +81,15 @@ class ReviewController extends Controller
      * @param  \App\Models\Review  $review
      * @return \Illuminate\Http\Response
      */
-    public function update(UpdateReviewRequest $request, Review $review)
+    public function update(Request $request,Product $product, Review $review)
     {
-        //
+        
+        $review->update($request->all());
+
+        return response([
+            'data' => new ReviewResource($review)
+        ]);
+
     }
 
     /**
