066932e6942c2a605ddb079d2385aeb35bdcd80c
Nikola Perisic <5dzoni5@gmail.com>
2022-07-10 23:50:03 +0200

    Small changes inside api routes

--- routes/api.php
+++ routes/api.php
@@ -5,10 +5,6 @@ use App\Http\Controllers\ReviewController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Resources\ProductCollection;
-use App\Http\Resources\ProductResource;
-use App\Models\Product;
-
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
